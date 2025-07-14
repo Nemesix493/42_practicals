83b86b76d5663f60b2e22c90634c9dc544a7088b
Nemesix493 <verquerre.kevin@gmail.com>
2025-07-14 21:22:14 +0200

    feat: add exercise C_05_ex04

---
+++ Pool/C_05/ex04/ft_fibonacci.c
@@ -0,0 +1,17 @@
+/* ********************************************* */
+/*                                               */
+/*                                               */
+/*   ft_fibonacci.c                              */
+/*                                               */
+/*   By: Nemesix493                              */
+/*                                               */
+/* ********************************************* */
+
+int ft_fibonacci(int index)
+{
+    if(index < 2)
+    {
+        return index;
+    }
+    return ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+}
