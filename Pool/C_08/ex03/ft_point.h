e9e2d83227cb5b5c2c369fa7dde59f8df0a00b8b
Nemesix493 <verquerre.kevin@gmail.com>
2025-07-17 00:25:24 +0200

    feat: add exercise C_08_ex03

---
+++ Pool/C_08/ex03/ft_point.h
@@ -0,0 +1,18 @@
+/* ********************************************* */
+/*                                               */
+/*                                               */
+/*   ft_point.h                                  */
+/*                                               */
+/*   By: Nemesix493                              */
+/*                                               */
+/* ********************************************* */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct {
+    int x;
+    int y;
+} t_point;
+
+#endif
